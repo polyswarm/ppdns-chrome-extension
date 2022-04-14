bba509c1ed1a4742b6e0e962f9a21e9172bc8c1c
Michael Bradford <michael.bradford@polyswarm.io>
2022-04-14 16:33:11 -0700

    formatting

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { SETTINGS_KEY, useSettingsStore } from '../../common/settings';
-
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
-
+import { SETTINGS_KEY } from '../../common/settings';
 class PpdnsBackground {
   constructor() {
     this.ppdnsL = new Map();
     this.ppdnsBatchSize = 10;
     // todo set from settings/opts
-
   }
 
   logPpdnsRequest(webRequestBody) {
@@ -46,49 +41,49 @@ class PpdnsBackground {
   }
 
   submitPpdnsBatch() {
-    // todo for mariano or victor, some better JS dev than me... doing it OK here?
     /* TODO ttl on ppdns resolutions ... there are alot of same IPs and this is rather high volume, almost want a bloom ttl */
 
     /* todo throw error on API key fail */
-
     chrome.storage.local.get(SETTINGS_KEY, this.cbX.bind(this));
-
   }
 
-  cbX(result){
+  cbX(result) {
     let data = {
       resolutions: Array.from(this.ppdnsL.values()),
     };
-    if(typeof result.settings === "undefined" || typeof result.settings.apiKey === "undefined"){
-          console.log('no settings in local store');
-          return;
-        }
-        let apiK = result.settings ? result.settings.apiKey : '';
-        if (apiK === undefined || apiK == '') {
-          console.log('failed to get API k from local storage');
-          return;
-        }
+    if (
+      typeof result.settings === 'undefined' ||
+      typeof result.settings.apiKey === 'undefined'
+    ) {
+      console.log('no settings in local store');
+      return;
+    }
+    let apiK = result.settings ? result.settings.apiKey : '';
+    if (apiK === undefined || apiK == '') {
+      console.log('failed to get API k from local storage');
+      return;
+    }
 
-        let baseUrl = 'http://localhost:9696/v3/ppdns';
-        let headers = {
-          // todo grab from settings!
-          Authorization: apiK,
-          'Content-Type': 'application/json',
-        };
-        // todo wanted to use axios, but it needs fetch adapter
-        fetch(baseUrl + '/ingest/', {
-          method: 'POST',
-          headers: headers,
-          body: JSON.stringify(data),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            this.ppdnsL.clear();
-            console.log(data);
-          })
-          .catch((error) => {
-            console.error('Error posting ingest: ', error);
-          });
+    let baseUrl = 'http://localhost:9696/v3/ppdns';
+    let headers = {
+      // todo grab from settings!
+      Authorization: apiK,
+      'Content-Type': 'application/json',
+    };
+    // todo wanted to use axios, but it needs fetch adapter
+    fetch(baseUrl + '/ingest/', {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.ppdnsL.clear();
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error posting ingest: ', error);
+      });
   }
 
   updateSettings(result) {
