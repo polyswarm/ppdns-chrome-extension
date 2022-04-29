9568640412c739aefb1b30381c113493c2dfebf9
Michael Bradford <michael.bradford@polyswarm.io>
2022-04-29 11:31:31 -0700

    read env batch size and disable safari build

--- utils/env.js
+++ utils/env.js
@@ -2,7 +2,6 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 3111,
-  AI_BASE_URL:
-    process.env.AI_BASE_URL || 'https://api.stage-new.polyswarm.network/',
+  AI_BASE_URL: process.env.AI_BASE_URL || 'http://artifact-index-e2e:9696',
   BATCH_SIZE: process.env.BATCH_SIZE || 10,
 };
