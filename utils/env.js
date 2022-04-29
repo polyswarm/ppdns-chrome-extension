90a40786a986f08c17b5257670458b3bc94c98e5
Michael Bradford <michael.bradford@polyswarm.io>
2022-04-29 11:31:11 -0700

    use stage api

--- utils/env.js
+++ utils/env.js
@@ -2,6 +2,7 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 3111,
-  AI_BASE_URL: process.env.AI_BASE_URL || 'http://artifact-index-e2e:9696',
+  AI_BASE_URL:
+    process.env.AI_BASE_URL || 'https://api.stage-new.polyswarm.network/',
   BATCH_SIZE: process.env.BATCH_SIZE || 10,
 };
