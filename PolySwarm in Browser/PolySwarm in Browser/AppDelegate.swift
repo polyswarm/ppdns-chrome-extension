47062cef274558c6c06049399674f33f3d60bd6d
Michael Bradford <michael.bradford@polyswarm.io>
2022-06-08 15:56:24 -0700

    initial safari commit

---
+++ PolySwarm in Browser/PolySwarm in Browser/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PolySwarm in Browser
+//
+//  Created by Polyswarm developer on 6/8/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
