90a0de7560af159fb2597516600d3c1a335051d0
Alan Justino <alan.justino@yahoo.com.br>
2024-07-11 17:57:50 -0300

    Tests generate the extension ID from folder path
    Had to derive a JS code from Python and C++ samples

--- index.test.js
+++ index.test.js
@@ -1,10 +1,35 @@
+
+const crypto = require('crypto');
+const path = require('node:path');
 const puppeteer = require('puppeteer');
-const { textSpanContainsPosition } = require('typescript');
 
 const EXTENSION_PATH = './build';
-const EXTENSION_ID = 'DEADBEEF';
+let EXTENSION_ID;
 
 let browser;
+
+/// Generates the Extension ID for Chrome-ish from its instalation path dir
+// Produced from samples in C++ and Python.
+// Samples From: https://stackoverflow.com/questions/26053434/how-is-the-chrome-extension-id-of-an-unpacked-extension-generated
+function getExtensionId(extensionPath) {
+    absolutePath = path.resolve(extensionPath);
+
+    // Calculate the SHA256 hash of the path
+    const hash = crypto.createHash('sha256').update(absolutePath, 'utf8').digest('hex');
+
+    // Take the first 32 characters of the hexadecimal hash
+    let extensionId = '';
+    for (let i = 0; i < 32; i++) {
+      // Convert each hex character to its decimal ASCII value, add 97 (ASCII for 'a'),
+      // and convert the result back to a character.
+      extensionId += String.fromCharCode(parseInt(hash[i], 16) + 97);
+    }
+
+    console.log(`Extension ID detected: ${extensionId} <- ${absolutePath}`);
+    return extensionId;
+}
+
+EXTENSION_ID = getExtensionId(EXTENSION_PATH);
 
 beforeEach(async () => {
     // TODO: Launch the browser;
