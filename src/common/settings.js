8b599f0a81b6bc9eaaf939ec49b1c97f662a450c
Alan Justino <alan.justino@yahoo.com.br>
2024-07-19 14:38:09 -0300

    Fix the check of "is settings initialized yet?"

--- src/common/settings.js
+++ src/common/settings.js
@@ -13,16 +13,20 @@ export const useSettingsStore = createChromeStorageStateHookLocal(
   INITIAL_VALUE
 );
 
-export const initStorage = (storage) => {
-  if (!!storage.get(SETTINGS_KEY)){
+export const initStorage = async (storage) => {
+  if (Object.keys(await storage.get(SETTINGS_KEY).length == 0)){
     let storage_map = {};
     storage_map[SETTINGS_KEY] = INITIAL_VALUE;
-    storage.set(storage_map);
+    await storage.set(storage_map);
   }
 };
 
 export const updateStorageField = async (storage, key, field, value) => {
   let storage_map = await storage.get(key);
+  if (Object.keys(storage_map).length == 0){
+    await initStorage(storage);
+    storage_map = await storage.get(key);
+  }
   storage_map[key][field] = value;
   return await storage.set(storage_map)
 };
